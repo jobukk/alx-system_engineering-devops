b1604293768391d6eeb3268d69e3cb3323ed5427
JobKoech610 <jobkoech610@gmail.com>
2024-06-19 09:14:06 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,18 @@
-from requests import get
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'my-custom-user-agent/0.0.1'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        response = get(url, headers=user_agent, allow_redirects=False)
-        if response.status_code == 200:
-            all_data = response.json()
-            return all_data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except Exception as e:
-        print(f"Error: {e}")
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
