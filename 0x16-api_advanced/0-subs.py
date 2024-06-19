a4b51233a1acad7d391cf168666716a81b9b254c
JobKoech610 <jobkoech610@gmail.com>
2024-06-19 09:17:14 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,28 @@
-#!/usr/bin/python3
-"""function that queries the Reddit API and returns the
-number of subscribers for a given subreddit"""
-
 import requests
+"""
+Importing requests module
+"""
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """
+    Function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0
+    except requests.RequestException:
         return 0
