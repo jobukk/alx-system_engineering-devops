9d3484c7e9135680dae8d8dcfec7e2fb328c4047
JobKoech610 <jobkoech610@gmail.com>
2024-06-29 17:34:00 +0300

    0x19-postmortem

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,13 +2,13 @@
 
 # Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
 
 #Increase soft file limit for user holberton
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
 
