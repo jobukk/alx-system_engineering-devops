c6336bb0a4b30ff1682a6e9a233b19ca5c6dd5f7
JobKoech610 <jobkoech610@gmail.com>
2024-06-29 17:35:43 +0300

    0x19-postmortem

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,5 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
-
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Fix open files
+exec { 'Fix hard limit':
+  command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
 }
 
